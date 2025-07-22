cb51554df09e945f38df1a5c16c773da5186b7d8
phihen <48790552+phihen@users.noreply.github.com>
2025-07-22 22:28:58 +0200

    add div elements for grid

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const gridContainer = document.querySelector('#grid-container');
-const divElements = new Array(16).fill(document.createElement('div'));
+const gridContainer = document.querySelector("#grid-container");
+
+for(let index = 0; index < 256; index++) {
+    const divElement = document.createElement("div");
+    gridContainer.appendChild(divElement);
+}
 
 
