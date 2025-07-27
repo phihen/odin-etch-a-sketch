c03694f93166cc277197fc442c3fb7485231c89f
phihen <48790552+phihen@users.noreply.github.com>
2025-07-27 19:03:54 +0200

    add reset button

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const gridContainer = document.querySelector("#grid-container");
+const resetButton = document.querySelector('#resetButton');
+const divElements = [];
+
+resetButton.addEventListener('click', (event) => {
+    divElements.forEach((element) => {
+        element.style.backgroundColor = "white";
+    })
+});
 
 for(let index = 0; index < 256; index++) {
     const divElement = document.createElement("div");
     divElement.addEventListener('mouseover', changeColor);
     gridContainer.appendChild(divElement);
+    divElements.push(divElement);
 }
 
 function changeColor(event) {
