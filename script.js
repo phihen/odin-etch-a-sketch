7f977bef5ea9c525ae580182d337be19dbc446c0
phihen <48790552+phihen@users.noreply.github.com>
2025-07-27 17:51:24 +0200

    add hover effect for grid

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ const gridContainer = document.querySelector("#grid-container");
 
 for(let index = 0; index < 256; index++) {
     const divElement = document.createElement("div");
+    divElement.addEventListener('mouseover', changeColor);
     gridContainer.appendChild(divElement);
+}
+
+function changeColor(event) {
+    event.target.style.backgroundColor = "black";
 }
 
 
